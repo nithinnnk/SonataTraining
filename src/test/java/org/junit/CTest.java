8bb9ec77ba9283166c090b29a84b716db739f7b9
nithin <nithin.koushik@sonata-software.com>
2022-08-25 14:07:47 +0530

    updated program

---
+++ src/test/java/org/junit/CTest.java
@@ -0,0 +1,24 @@
+package org.junit;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import com.example.demo.impl.Calculator;
+
+public class CTest {
+	@Test
+	public void divTest1() {
+		
+		Calculator c1 = new Calculator();
+		int x = c1.div(2,2);
+		Assertions.assertThrows(ArithmeticException.class,()->c1.div(2,0),"this is an error");
+	}
+	@Test
+	public void divTest2() {
+		
+		Calculator c1 = new Calculator();
+		int x = c1.div(2,2);
+		Assertions.assertThrows(ArithmeticException.class,()->c1.div(2,1),"this is an error");
+	}
+
+}
